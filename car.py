9cae0b19dc7ce85d6697d261b1e1a2af578763fb
LeonGora <33520119+LeonGora@users.noreply.github.com>
2017-12-21 18:10:51 +0300

    Add files via upload

---
+++ car.py
@@ -0,0 +1,25 @@
+
+
+a = {'S': 1,
+     't': None,
+     'V': None}
+b = {'S': 1,
+     't': None,
+     'V': None}
+S = None
+
+t = None
+
+V_average = None
+
+a['t'] = a['s'] / a['V']
+
+b['t'] = b['s'] / b['V']
+
+s = a['s'] + b['s']
+
+t = a['s'] + b['s']
+
+V_average = S/t
+
+
